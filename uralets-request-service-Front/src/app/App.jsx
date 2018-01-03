25a21120ec2af5df34f637db7daa01ddd07bdbf6
dimaname <dimaname@gmail.com>
2018-01-04 04:18:41 +0500

    isAuth to route

--- uralets-request-service-Front/src/app/App.jsx
+++ uralets-request-service-Front/src/app/App.jsx
@@ -3,19 +3,19 @@ import './App.css';
 import RequestList from '../requestList/requestList';
 import LoginPage from '../loginPage/loginPage';
 import Header from '../header/header';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 class App extends Component {
   render() {
-    return (
-      <div>
-       <Header/>
-       <Switch>       
-        <Route path="/login" exact component={LoginPage}/>    
-        <Route path="/" component={RequestList}/>    
-       </Switch>     
-      </div>
-    );
+     
+    const isAuth = false;
+      return <div> 
+        {isAuth && <Header/> }
+        <Switch>       
+         <Route path="/login" exact component={LoginPage}/>    
+         <Route path="/" render={props => (isAuth ? (<RequestList/>) : (<Redirect to='/login'/>) )}/>    
+        </Switch>           
+      </div>   
   }
 }
 
